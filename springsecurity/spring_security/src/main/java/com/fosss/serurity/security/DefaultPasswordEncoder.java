9cc587df65a5e3264921ea5643d7312f48676719
fo <1745179058@qq.com>
2022-09-16 21:04:31 +0800

    springsecurity/

---
+++ springsecurity/spring_security/src/main/java/com/fosss/serurity/security/DefaultPasswordEncoder.java
@@ -0,0 +1,42 @@
+package com.fosss.serurity.security;
+
+import com.fosss.commonutils.MD5;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * t密码的处理方法类型
+ * </p>
+ *
+ * @author qy
+ * @since 2019-11-08
+ */
+@Component
+public class DefaultPasswordEncoder implements PasswordEncoder {
+
+    public DefaultPasswordEncoder() {
+        this(-1);
+    }
+
+    /**
+     * @param strength
+     *            the log rounds to use, between 4 and 31
+     */
+    public DefaultPasswordEncoder(int strength) {
+
+    }
+
+    public String encode(CharSequence rawPassword) {
+        /* TODO 推荐这种springsecurity自带的加密方式
+        BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        return passwordEncoder.encode(rawPassword);
+        */
+        return MD5.encrypt(rawPassword.toString());
+    }
+
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodedPassword.equals(MD5.encrypt(rawPassword.toString()));
+    }
+}
