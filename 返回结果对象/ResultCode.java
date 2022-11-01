d2aa8245855c509c7090622be173d25e40624219
fo <1745179058@qq.com>
2022-11-01 21:02:42 +0800

    resultCode

---
+++ 返回结果对象/ResultCode.java
@@ -0,0 +1,12 @@
+package com.fosss.commonutils;
+
+import lombok.Data;
+
+/**
+ * 封装成功失败的响应码
+ */
+public interface ResultCode {
+    public static Integer SUCCESS=20000;
+    public static Integer ERROR=20001;
+
+}
